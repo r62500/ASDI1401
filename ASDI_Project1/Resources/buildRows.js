9908c1d8dca5d12561936dd9810fd1ce0ef9ff7f
Aaron Korpa <aaronkorpa@Aarons-MacBook-Pro.local>
2014-01-09 16:29:47 -0800

    Commented out all of the items to describe the process and logic.

--- ASDI_Project1/Resources/buildRows.js
+++ ASDI_Project1/Resources/buildRows.js
@@ -2,8 +2,10 @@
 // ASDI Term 1401
 // Project 1
 
+// Load in object data from data.js file.
 var objData = require("data");
 
+// Loop thru array to create rows containing all of the object data.
 exports.rows = function createObjectRows() {
 	for(i=0;i<objData.data.length;i++){
 		var row = Ti.UI.createTableViewRow({
